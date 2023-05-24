f5270f7966a8eac15760f710f3edd0f7c9583e78
Ra-Liz <2879055132@qq.com>
2023-05-24 16:42:26 +0800

    docs: 修改了一些文档内容

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  productionSourceMap: false, // 把map文件拿掉了，看不到报错行数啥的了
   transpileDependencies: true,
   lintOnSave: false, //并无卵用，用了插件，不过用ESLint查错挺好的
   devServer: {
